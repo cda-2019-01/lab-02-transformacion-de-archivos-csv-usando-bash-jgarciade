3f7cbba7e2dc4135d006ea3fd09df0516bbace01
jose garcia <jgarciade@unal.edu.co>
2019-05-15 03:39:28 +0000

    initial commit

--- script.sh
+++ script.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/bash
+for file in estaciones/*.csv
+  do
+    cat "$file" | \
+    # Transforms , for . as decimal separator
+    sed 's/:\([0-9]*\)\;\([0-9]*\)\;\([0-9]\),\([0-9]*\)/:\1;\2;\3.\4/' | \
 
+    # Calculates mean
+    awk '/^[0-9]*\/[0-9]*\/06\;/ { print } '| \
+    awk '/^[0-9]*\/[0-9]*\/06\;/ { split($0,fields,";"); sum += fields[3]; len += 1 }
+         END {printf "%s;%s---", fields[1], sum/(len+1)}'
+done
